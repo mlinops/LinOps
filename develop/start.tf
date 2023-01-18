8ca27afa14f4e321001ab8b85e43ef819020d088
Lina Duko <lina.infobox@gmail.com>
2023-01-18 14:24:37 +0300

    gcp

--- develop/start.tf
+++ develop/start.tf
@@ -19,15 +19,15 @@ resource "google_compute_instance" "server" {
             network = "default"
         }
 
-        shielded_instance_config {
-            # Defines whether the instance has integrity monitoring enabled. 
-            # Enables monitoring and attestation of the boot integrity of the instance. 
-            # The attestation is performed against the integrity policy baseline. 
-            # This baseline is initially derived from the implicitly trusted boot image when the instance is created.
-            enable_integrity_monitoring = "default" 
-            enable_vtpm = "default"
-        }
-}
+#         shielded_instance_config {
+#             # Defines whether the instance has integrity monitoring enabled. 
+#             # Enables monitoring and attestation of the boot integrity of the instance. 
+#             # The attestation is performed against the integrity policy baseline. 
+#             # This baseline is initially derived from the implicitly trusted boot image when the instance is created.
+#             enable_integrity_monitoring = "default" 
+#             enable_vtpm = "default"
+#         }
+# }
 
 # resource "google_compute_instance" "server-2" {
 #     name = "gcp-server-2"
