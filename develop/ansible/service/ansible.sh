ab4284185466ff3b57d9bbb5f9256311e2a7c72e
Lina Duko <lina.infobox@gmail.com>
2023-01-22 16:52:39 +0300

    complete

--- develop/ansible/service/ansible.sh
+++ develop/ansible/service/ansible.sh
@@ -5,8 +5,8 @@ sudo apt update
 sudo apt-get -y install ansible makepasswd curl
 
 # Set up SSH for ansible:
-sudo ssh-keygen -f /root/.ssh/ansible -q -N ""
-sudo bash -c 'cat /root/.ssh/ansible.pub > /root/.ssh/authorized_keys'
+sudo ssh-keygen -f /root/.ssh/id_rsa -q -N ""
+sudo bash -c 'cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys'
 sudo sed -i 's/PermitRootLogin prohibit_password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo sed -i '/PermitRootLogin yes/s/^#//g' /etc/ssh/sshd_config
 sudo /etc/init.d/ssh restart
@@ -20,7 +20,7 @@ curl -fsSL https://raw.githubusercontent.com/linaduko/LinOps/develop/develop/ans
 sudo mv /tmp/hosts /etc/ansible/hosts 
 
 # Pull the playbook-file for ansible
-https://raw.githubusercontent.com/linaduko/LinOps/develop/develop/ansible/playbook-main.yml -o /tmp/playbook-main.yml
+curl -fsSL https://raw.githubusercontent.com/linaduko/LinOps/develop/develop/ansible/playbook-main.yml -o /tmp/playbook-main.yml
 sudo ansible-playbook /tmp/playbook-main.yml
 
-systemctl is-active --quiet nginx && echo Service is running > test.txt
+#systemctl is-active --quiet nginx && echo Service is running > test.txt
