5d542d51f735788152d2b89e9713f48fbd452ed9
Lina Duko <lina.infobox@gmail.com>
2023-01-24 21:36:02 +0300

    test

--- develop/start.tf
+++ develop/start.tf
@@ -68,8 +68,6 @@ resource "google_compute_instance" "server" {
 
 }
 
-
-
 #    service_account {
 #      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
 #      email  = google_service_account.default.email
@@ -77,29 +75,29 @@ resource "google_compute_instance" "server" {
 #    }
 
 
-resource "google_compute_instance" "server-2" {
-    name = "gcp-server-2"
-    machine_type = "e2-micro"
+# resource "google_compute_instance" "server-2" {
+#     name = "gcp-server-2"
+#     machine_type = "e2-micro"
 
-    shielded_instance_config {
-        enable_vtpm = true
-        enable_integrity_monitoring = true
-    }
+#     shielded_instance_config {
+#         enable_vtpm = true
+#         enable_integrity_monitoring = true
+#     }
 
-    boot_disk {
-        initialize_params {
-            image = "centos-7-v20200403"
-        }
-    }
-        network_interface {
-            network = "default"
-            network_ip = "10.128.0.12"
-        }
+#     boot_disk {
+#         initialize_params {
+#             image = "centos-7-v20200403"
+#         }
+#     }
+#         network_interface {
+#             network = "default"
+#             network_ip = "10.128.0.12"
+#         }
 
-    metadata = {
-        block-project-ssh-keys = true
-    }
+#     metadata = {
+#         block-project-ssh-keys = true
+#     }
 
-    metadata_startup_script = "curl -fsSL https://raw.githubusercontent.com/linaduko/LinOps/develop/develop/ansible/service/ansible-start-centos.sh -o /tmp/ansible.sh; sudo bash /tmp/ansible.sh"
-}
+#     metadata_startup_script = "curl -fsSL https://raw.githubusercontent.com/linaduko/LinOps/develop/develop/ansible/service/ansible-start-centos.sh -o /tmp/ansible.sh; sudo bash /tmp/ansible.sh"
+# }
 
