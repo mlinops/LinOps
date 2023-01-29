a903225cb6d7ce52f0932ecde90f4718f33ed329
Lina Duko <lina.infobox@gmail.com>
2023-01-29 21:09:02 +0300

    test

---
+++ tests/hosts/dev.sh
@@ -0,0 +1,20 @@
+if ping -c 1 gcp-server-1-dev &> /dev/null
+then
+  echo "SUCCESS"
+else
+  echo "ERROR"
+fi
+
+if ping -c 1 gcp-server-2-dev &> /dev/null
+then
+  echo "SUCCESS"
+else
+  echo "ERROR"
+fi
+
+if ping -c 1 gcp-server-3-dev &> /dev/null
+then
+  echo "SUCCESS"
+else
+  echo "ERROR"
+fi
