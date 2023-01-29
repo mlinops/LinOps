5a6e941eee138811cb1bff15a530245dc9589be8
Lina Duko <lina.infobox@gmail.com>
2023-01-29 22:21:38 +0300

    test

--- tests/hosts/dev/site_check.sh
+++ tests/hosts/dev/site_check.sh
@@ -1,11 +1,8 @@
-header=$(curl -I http://gcp-server-1-dev | head -n 1 | cut -c 10-18)
+header=$(curl -I http://gcp-server-1-dev | head -n 1 | cut -c 10-15)
 
 if [ $header == '200 OK' ]
 then 
 echo SITE 1 OK
-elif [ $header == '302 Found' ]
-then 
-echo SITE 1 FOUND
 else
 echo SITE 1 NOT OK
 exit 1
