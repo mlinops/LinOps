67a24af21a61b48fa6c0517256c6cd184f4b564d
Lina Duko <lina.infobox@gmail.com>
2023-01-29 20:35:39 +0300

    test

--- tests/terraform/t_version.sh
+++ tests/terraform/t_version.sh
@@ -1,10 +1,6 @@
 terraform_version_command=$(terraform version)
-terver=$(echo $terraform_version_command | head -n  1 | cut -c 12-16)
-provider=$(echo $terraform_version_command | head -n  1| cut -c 76-81)
-provider_version=$(echo $terraform_version_command | head -n  1 | cut -c 84-89)
+terver=$(echo $terraform_version_command | head -n  1| cut -c 12-16)
 etalon_version=$(echo "1.3.7")
-etalon_provider=$(echo "google")
-etalon_provider_version=$(echo "4.50.0")
 
 if [ $terver == $etalon_version ]
 then
@@ -13,21 +9,3 @@ else
 echo Terraform version NOT OK
 exit 1
 fi
-
-if [ $provider == $etalon_provider ]
-then
-echo Terraform provider OK
-else
-echo Terraform provider NOT OK
-exit 1
-fi
-
-if [ $provider_version == $etalon_provider_version ]
-then
-echo Terraform provider version OK
-else
-echo Terraform provider version NOT OK
-exit 1
-fi
-
-
