b52133c1f49909b7749a7943a8565e96db993fe0
Lina Duko <lina.infobox@gmail.com>
2023-01-30 15:21:21 +0300

    test

--- tests/terraform/t_version.sh
+++ tests/terraform/t_version.sh
@@ -2,6 +2,8 @@ terraform_version_command=$(terraform version)
 terver=$(echo $terraform_version_command | head -n  1| cut -c 12-16)
 etalon_version=$(echo "1.3.7")
 
+terraform version
+
 if [ $terver == $etalon_version ]
 then
 echo Terraform version OK
